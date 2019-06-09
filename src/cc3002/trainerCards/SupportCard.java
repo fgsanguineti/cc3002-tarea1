c27462621224ea9dbf7514a52c5e8e6357ded6ad
F. Giovanni Sanguineti <franco.sanguineti@ug.uchile.cl>
2019-06-09 18:00:06 -0400

    updated TrainerCards

--- src/cc3002/trainerCards/SupportCard.java
+++ src/cc3002/trainerCards/SupportCard.java
@@ -1,10 +1,14 @@
 package cc3002.trainerCards;
+
 import cc3002.effectVisitor.SupportEffectVisitor;
+
 import java.util.ArrayList;
 
 public class SupportCard  extends AbstractTrainerCard{
     private ArrayList<SupportEffectVisitor> effects;
-    SupportCard(String cardName, String description) {
+
+    SupportCard(String cardName, String description, ArrayList<SupportEffectVisitor> effects) {
         super(cardName, description);
+        this.effects = effects;
     }
 }
