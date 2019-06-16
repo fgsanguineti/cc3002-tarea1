ec5e2a9846ca8400aa01013675cd707c1a656763
F. Giovanni Sanguineti <franco.sanguineti@ug.uchile.cl>
2019-06-16 03:18:18 -0400

    updated ability behavior for visitor

---
+++ test/cc3002/abilities/EnergyBurnTest.java
@@ -0,0 +1,12 @@
+package cc3002.abilities;
+
+import org.junit.Before;
+
+public class EnergyBurnTest {
+    EnergyBurn energyBurn = new EnergyBurn();
+
+    @Before
+    public void setUp() {
+    }
+
+}
